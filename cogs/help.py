ea425ceeaa39988b95489a50f0397bb5ea0aec9e
NotTheNobleSavage <54433235+NotTheNobleSavage@users.noreply.github.com>
2023-01-28 09:16:15 +0800

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -13,7 +13,10 @@ class Help(commands.Cog):
         The features below deal with how you want to be alerted when an emission occurs. These commands can only be run by admins! \n
         ** /add_alert region: ** \n This creates an alert for the region in the current channel in which the command is run. You can set the region to a specific region or can enter “all” to get alerts for all regions. \n
         ** /view_alert: ** \n This views all the current alerts that are set up in this channel. \n
-        ** /remove_alert region: ** \n This command is used to remove an alert for region specified in the current channel.
+        ** /remove_alert region: ** \n This command is used to remove an alert for region specified in the current channel. \n
+        ** /help ** \n This can be used to run this command and view help info.\n
+        ** /clans region: ** \n Used to view all the clans in the current regions, only works in RU right now. \n
+        ** /current_lots item_name:, region: ** \n Can be used to find the current auctions for the item specified in the region.
         """)
         await ctx.respond(embed=embed)
         return            
